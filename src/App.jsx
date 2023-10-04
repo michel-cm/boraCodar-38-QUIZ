3f5fa36002426a15e3f1eaea235b0969614c9b44
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-10-04 13:54:26 -0300

    score inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import { Header } from "./components/Header";
 
+const INITIAL_SCORE = 0;
+
 export function App() {
+  const [score, setScore] = useState(INITIAL_SCORE);
 
   return (
   <div className="flex flex-col h-full pt-[62px] max-w-[842px] m-auto gap-[69px]">
-    <Header />
+    <Header correctAnswers={score} />
   </div>
   )
 }
