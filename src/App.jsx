3c854fd1e89d78eb2af75ddb02a776f06f7d15cf
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-10-04 13:52:24 -0300

    add data / feate header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Header } from "./components/Header";
+
 export function App() {
 
   return (
-   <h1>QUIZ</h1>
+  <div className="flex flex-col h-full pt-[62px] max-w-[842px] m-auto gap-[69px]">
+    <Header />
+  </div>
   )
 }
 
---
+++ src/data.js
@@ -0,0 +1,59 @@
+export const questions = [
+    {
+        id: 1,
+        question: "1. Qual era o objetivo do personagem?",
+        options: [
+            {
+                id: "image-1",
+                text: "Caçar tesouros"
+            },
+            {
+                id: "image-2",
+                text: "Ter mais tempo para ler livros"
+            },
+            {
+                id: "image-3",
+                text: "Navegar pelos 7 mares"
+            },
+        ],
+        correctAnswer: "Ter mais tempo para ler livros"
+    },
+    {
+        id: 2,
+        question: "2. Qual era o objetivo do personagem?",
+        options: [
+            {
+                id: "image-1",
+                text: "Caçar tesouros"
+            },
+            {
+                id: "image-2",
+                text: "Ter mais tempo para ler livros"
+            },
+            {
+                id: "image-3",
+                text: "Navegar pelos 7 mares"
+            },
+        ],
+        correctAnswer: "Ter mais tempo para ler livros"
+    },
+    {
+        id: 3,
+        question: "3. Qual era o objetivo do personagem?",
+        options: [
+            {
+                id: "image-1",
+                text: "Caçar tesouros"
+            },
+            {
+                id: "image-2",
+                text: "Ter mais tempo para ler livros"
+            },
+            {
+                id: "image-3",
+                text: "Navegar pelos 7 mares"
+            },
+        ],
+        correctAnswer: "Ter mais tempo para ler livros"
+    },
+]
